07d902d2a738a8089bc77bfa9806c216f9200753
jeremiandrade <jeremiandrade04@gmail.com>
2026-03-03 23:31:36 +0100

    add ex n2

--- arrays_1/01-arrays/script.js
+++ arrays_1/01-arrays/script.js
@@ -14,12 +14,14 @@ const fourthTeacher = teachers[3] ;
 console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers[4] = 'Patrick' 
+console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = ;
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
