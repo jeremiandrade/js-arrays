04e33d3ad3818f95c7b1abed9d3aabecc1f5a11e
jeremiandrade <jeremiandrade04@gmail.com>
2026-03-03 23:49:58 +0100

    add ex 4 and 5

--- arrays_1/01-arrays/script.js
+++ arrays_1/01-arrays/script.js
@@ -18,12 +18,17 @@ teachers[4] = 'Patrick'
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = 
+const lastTeacher = teachers.pop()
+console.log(lastTeacher);
+
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = ;
+const firstTeacher = teachers.shift()
+console.log(firstTeacher);
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
